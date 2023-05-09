83c87a86de9632edb1f68aaa43b4e885e3d0de20
Fitra R <39044004+Rizki36@users.noreply.github.com>
2023-05-09 22:17:01 +0700

    fix: update banks code

--- src/resources/payment/payment.validation.ts
+++ src/resources/payment/payment.validation.ts
@@ -2,20 +2,7 @@ import z from "zod";
 
 export const createVaSchema = z.object({
   body: z.object({
-    bankCode: z.enum([
-      "BCA",
-      "BNI",
-      "MANDIRI",
-      "PERMATA",
-      "SAHABAT_SAMPOERNA",
-      "BRI",
-      "BNC",
-      "CIMB",
-      "BSI",
-      "BJB",
-      "DBS",
-      "DBS",
-    ]),
+    bankCode: z.enum(["BCA", "BNI", "MANDIRI", "BRI"]),
     name: z.string(),
     amount: z.number().min(10000).max(100000000),
   }),
